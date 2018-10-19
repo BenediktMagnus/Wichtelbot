717f2fcf7752edc370a519c357b47cb4cde382c5
Benedikt Magnus <magnus@magnuscraft.de>
2018-10-19 17:06:43 +0200

    Nutzerhandhabung für Datenbank.

--- Skripte/Datenbank.js
+++ Skripte/Datenbank.js
@@ -16,7 +16,58 @@ exports.Initialisieren = function ()
 {
     DatenbankWichteln = VerbindeMitDatenbank('Wichteln');
     DatenbankLog = VerbindeMitDatenbank('Log');
-}
+};
+
+/**
+ * @param {Object} Nutzer Das Nutzerobjekt mit allen Angaben zum Nutzer.
+ */
+exports.NutzerSpeichern = function (Nutzer)
+{
+    DatenbankWichteln.run(
+        'INSERT INTO Nutzer (NutzerId, Discord, Name, Nickname, Zeit, Zustand) VALUES (?, ?, ?, ?, ?, ?)',
+        Nutzer.Id,
+        Nutzer.Discord,
+        Nutzer.Name,
+        Nutzer.Nickname,
+        AktuelleUnixzeit(),
+        Nutzer.Zustand,
+        Fehlerbehandlung
+    );
+};
+
+/**
+ * Lädt einen Nutzer aus der Datenbank.
+ * @param {Number} NutzerId Die Discord-ID des Nutzers.
+ * @param {Function} Callback Callback, der nach dem Laden des Nutzers ausgeführt wird. Parameter: {Object} Reihe.
+ */
+exports.NutzerLaden = function (NutzerId, Callback)
+{
+    DatenbankWichteln.get(
+        'SELECT * FROM Nutzer WHERE NutzerId = ?',
+        NutzerId,
+        function (Fehler, Reihe)
+        {
+            Fehlerbehandlung(Fehler);
+            Callback(Reihe);
+        }                    
+    );
+};
+
+/**
+ * Lädt alle Nutzer aus der Datenbank.
+ * @param {Function} Callback Callback, der für jeden gefundenen Nutzer ausgeführt wird. Parameter: {Object} Reihe.
+ */
+exports.AlleNutzerLaden = function (Callback)
+{
+    DatenbankWichteln.each(
+        'SELECT * FROM Nutzer',
+        function (Fehler, Reihe)
+        {
+            Fehlerbehandlung(Fehler);
+            Callback(Reihe);
+        }
+    );
+};
 
 /**
  * Loggt die Eingabe eines Nutzers.
@@ -26,13 +77,14 @@ exports.Initialisieren = function ()
  */
 exports.Log = function (NutzerId, Name, Eingabe)
 {
-    DatenbankLog.run('INSERT INTO Log (NutzerId, Name, Eingabe, Zeit) VALUES (?, ?, ?, ?)',
-                     NutzerId,
-                     Name,
-                     Eingabe,
-                     Math.floor(new Date() / 1000)
+    DatenbankLog.run(
+        'INSERT INTO Log (NutzerId, Name, Eingabe, Zeit) VALUES (?, ?, ?, ?)',
+        NutzerId,
+        Name,
+        Eingabe,
+        AktuelleUnixzeit()
     );
-}
+};
 
 /**
  * Erzeugt eine neue Datenbankverbindung.
@@ -61,4 +113,13 @@ function Fehlerbehandlung (Fehler)
 {
     if (Fehler)
         console.error(Fehler);
+}
+
+/**
+ * Gibt die aktuelle Unixzeit zurück.
+ * @returns {Number} Die aktuelle Unixzeit in Sekunden.
+ */
+function AktuelleUnixzeit ()
+{
+    return Math.floor(new Date() / 1000);
 }
