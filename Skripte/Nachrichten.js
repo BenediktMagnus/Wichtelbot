dd58aa71bab485fe99031b7440837682a290a92e
Benedikt Magnus <magnus@magnuscraft.de>
2018-11-01 15:39:35 +0100

    Gesprächsverlauf nun vollständig.

--- Skripte/Nachrichten.js
+++ Skripte/Nachrichten.js
@@ -72,10 +72,28 @@ const Definitionen = {
             }
         },
         Anschrift: {
-            Datenaufnahme: true
+            Datenaufnahme: true,
+            Funktion: DatenAufnehmen,
+            Ziel: "Land",
+            Text: Texte.Land
+        },
+        Land: {
+            "deutschland": {
+                Funktion: LandVerarbeiten
+            },
+            "österreich": {
+                Funktion: LandVerarbeiten
+            },
+            "schweiz": {
+                Funktion: LandVerarbeiten
+            },
+            "luxemburg": {
+                Funktion: LandVerarbeiten
+            }
         },
         Steam: {
-            Datenaufnahme: true
+            Datenaufnahme: true,
+            Funktion: SteamVerarbeiten
         },
         International: {
             "ja": {
@@ -84,23 +102,38 @@ const Definitionen = {
                 Text: Texte.Wunschliste
             },
             "nein": {
-                
+                Funktion: DatenAufnehmen,
+                Ziel: "Wunschliste",
+                Text: Texte.Wunschliste
             }
         },
         Wunschliste: {
-            Datenaufnahme: true
+            Datenaufnahme: true,
+            Funktion: WunschlisteVerarbeiten
         },
         Allergien: {
-            Datenaufnahme: true
+            Datenaufnahme: true,
+            Funktion: DatenAufnehmen,
+            Ziel: "AusschlussGeschenk",
+            Text: Texte.AusschlussGeschenk
         },
         AusschlussGeschenk: {
-            Datenaufnahme: true
+            Datenaufnahme: true,
+            Funktion: DatenAufnehmen,
+            Ziel: "AusschlussWichtel",
+            Text: Texte.AusschlussWichtel
         },
         AusschlussWichtel: {
-            Datenaufnahme: true
+            Datenaufnahme: true,
+            Funktion: DatenAufnehmen,
+            Ziel: "Freitext",
+            Text: Texte.Freitext
         },
         Freitext: {
-            Datenaufnahme: true
+            Datenaufnahme: true,
+            Funktion: DatenAufnehmen,
+            Ziel: "Teilnehmer",
+            Text: Texte.Teilnehmer
         },
         Teilnehmer: {
 
@@ -221,7 +254,6 @@ function Fortfahren (Nachricht, Nutzer, Befehlsobjekt)
  */
 function DatenAufnehmen (Nachricht, Nutzer, Befehlsobjekt)
 {
-
     //Wenn der Zustand jeden Text aufnimmt, muss dieser in seiner Beschaffenheit erhalten bleiben.
     //Gibt es jedoch einen spezifischen Befehl, also wenn keine pauschale Datenaufnahme stattfindet, so wird dieser eingetragen,
     //da er bereits verarbeitet wurde (nur Kleinbuchstaben, Punkt am Ende abgetrennt etc.).
@@ -245,4 +277,90 @@ function Registrieren (Nachricht, Nutzer)
 
     Nachricht.reply(Texte.Registriert);
     Nachricht.reply(Texte.Regeln);
+}
+
+/**
+ * Verarbeitet die Eingabe des eigenen Landes und schickt den Nutzer zur nächsten Frage.
+ * @param {Object} Nachricht Die Nachricht, die per Discord erhalten wurde, ein Discordnachrichtenobjekt.
+ * @param {Object} Nutzer Das Nutzerobjekt mit allen Angaben zum Nutzer.
+ */
+function LandVerarbeiten (Nachricht, Nutzer)
+{
+    Nutzer[Nutzer.Zustand] = Nachricht.content;
+
+    let Antworttext = '';
+
+    if (Nutzer.AnalogDigitalSelbst == 'beides')
+    {
+        Nutzer.Zustand = 'Steam';
+        Antworttext = Texte.Steam;
+    }
+    else if (Nutzer.AnalogDigitalWichtel != 'digital')
+    {  
+        Nutzer.Zustand = 'International';
+        Antworttext = Texte.International;
+    }
+    else
+    {
+        Nutzer.Zustand = 'Wunschliste';
+        Antworttext = Texte.Wunschliste;
+    }
+
+    Nutzerverwaltung.Aktualisieren(Nutzer);
+
+    Nachricht.reply(Antworttext);
+}
+
+/**
+ * Verarbeitet die Eingabe eines Steamnamens und schickt den Nutzer zur nächsten Frage.
+ * @param {Object} Nachricht Die Nachricht, die per Discord erhalten wurde, ein Discordnachrichtenobjekt.
+ * @param {Object} Nutzer Das Nutzerobjekt mit allen Angaben zum Nutzer.
+ */
+function SteamVerarbeiten (Nachricht, Nutzer)
+{
+    Nutzer[Nutzer.Zustand] = Nachricht.content;
+
+    let Antworttext = '';
+
+    if (Nutzer.AnalogDigitalWichtel != 'digital')
+    {  
+        Nutzer.Zustand = 'International';
+        Antworttext = Texte.International;
+    }
+    else
+    {
+        Nutzer.Zustand = 'Wunschliste';
+        Antworttext = Texte.Wunschliste;
+    }
+
+    Nutzerverwaltung.Aktualisieren(Nutzer);
+
+    Nachricht.reply(Antworttext);
+}
+
+/**
+ * Verarbeitet die Eingabe einer Wunschliste und schickt den Nutzer zur nächsten Frage.
+ * @param {Object} Nachricht Die Nachricht, die per Discord erhalten wurde, ein Discordnachrichtenobjekt.
+ * @param {Object} Nutzer Das Nutzerobjekt mit allen Angaben zum Nutzer.
+ */
+function WunschlisteVerarbeiten (Nachricht, Nutzer)
+{
+    Nutzer[Nutzer.Zustand] = Nachricht.content;
+
+    let Antworttext = '';
+
+    if (Nutzer.AnalogDigitalSelbst != 'digital')
+    {  
+        Nutzer.Zustand = 'Allergien';
+        Antworttext = Texte.Allergien;
+    }
+    else
+    {
+        Nutzer.Zustand = 'AusschlussGeschenk';
+        Antworttext = Texte.AusschlussGeschenk;
+    }
+
+    Nutzerverwaltung.Aktualisieren(Nutzer);
+
+    Nachricht.reply(Antworttext);
 }
