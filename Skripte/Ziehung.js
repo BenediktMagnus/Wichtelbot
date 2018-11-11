688eda47749aba05dec4c138041cc757d92ec3aa
Benedikt Magnus <magnus@magnuscraft.de>
2018-11-11 18:27:34 +0100

    Ziehungsmodul.

---
+++ Skripte/Ziehung.js
@@ -0,0 +1,25 @@
+var Nutzerverwaltung;
+var Datenbankverwaltung;
+
+/**
+ * Der Klient, der sich mit Discord verbunden hat.
+ */
+var Klient;
+
+/**
+ * Initialisiert die Ziehung/Losung der Wichtel.
+ * @param {Object} Nutzerbibliothek
+ * @param {Object} Datenbankbibliothek
+ * @param {Object} NeuerKlient Der Discordklient, über den sich der Bot verbunden hat.
+ */
+exports.Initialisieren = function (Nutzerbibliothek, Datenbankbibliothek, NeuerKlient)
+{
+    Nutzerverwaltung = Nutzerbibliothek;
+    Datenbankverwaltung = Datenbankbibliothek;
+    Klient = NeuerKlient;
+};
+
+exports.Ausführen = function (ZiehungAusgeführt)
+{
+
+};
