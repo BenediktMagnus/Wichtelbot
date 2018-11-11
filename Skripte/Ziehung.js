7d3c7da6cdb3e29f134e7fbdc2cbda5cd6a88dea
Benedikt Magnus <magnus@magnuscraft.de>
2018-11-11 18:30:35 +0100

    Ermitteln der notwendigen Daten für die Ziehung.

--- Skripte/Ziehung.js
+++ Skripte/Ziehung.js
@@ -21,5 +21,32 @@ exports.Initialisieren = function (Nutzerbibliothek, Datenbankbibliothek, NeuerK
 
 exports.Ausführen = function (ZiehungAusgeführt)
 {
+    let Teilnehmerliste = new Map();
 
+    //Alle Teilnehmer in die Teilnehmerliste füllen:
+    for (let Nutzer of Nutzerverwaltung.Liste.values())
+        if (Nutzer.Zustand == 'Teilnehmer')
+            Teilnehmerliste.set(Nutzer.Id, Nutzer);
+
+    /**
+     * Ordnet Nutzern einer möglichen Liste an Wichtelpartnern zu, um diese später zu ermitteln.
+     */
+    let Zuordnungsliste = new Map();
+
+    //Zuordnungsliste wird gefüllt:
+    for (let Teilnehmer of Teilnehmerliste.values())
+    {
+        let Wichtelliste = new Map(Teilnehmerliste);
+        Wichtelliste.delete(Teilnehmer.Id); //Aktuellen Teilnehmer entfernen.
+
+        Zuordnungsliste.set(Teilnehmer.Id, {
+                Nutzer: Teilnehmer,
+                Wichtel: Wichtelliste,
+                Ausschlüsse: {
+                    Wunsch: [],
+                    Wichtel: [] //War Wichtel des Teilnehmers in vorherigen Jahren.
+                },
+            }
+        );
+    }
 };
