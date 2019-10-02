ff592d438afe3a9fda1c90f43450b97c4959e673
Benedikt Magnus <magnus@magnuscraft.de>
2019-10-02 21:40:20 +0200

    Mocha test framework

---
+++ tests/_mochaTest.ts
@@ -0,0 +1,12 @@
+import 'mocha';
+import { equal } from 'assert';
+
+describe('_ Mocha',
+    () => {
+        it('is working.',
+            () => {
+                equal(true, true);
+            }
+        );
+    }
+);
