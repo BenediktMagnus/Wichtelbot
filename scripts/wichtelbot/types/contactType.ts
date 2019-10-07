154e4d9867dcbc5805b43a4c4d79858529c24c31
Benedikt Magnus <magnus@magnuscraft.de>
2019-10-07 20:49:57 +0200

    Contact type documentation

--- scripts/wichtelbot/types/contactType.ts
+++ scripts/wichtelbot/types/contactType.ts
@@ -1,8 +1,17 @@
 enum ContactType
 {
-    Contact = 'contact', // After first contact
-    Member = 'member', // After registration
-    Wichtel = 'wichtel', // After the assignment has happened
+    /**
+     * After first contact, only contains basic contact data.
+     */
+    Contact = 'contact',
+    /**
+     * After first full registration. Besides the contact data, these ones have information attached.
+     */
+    Member = 'member',
+    /**
+     * After the assignment has happened, includes contact data, information and gift giver/taker IDs.
+     */
+    Wichtel = 'wichtel',
 }
 
 export default ContactType;
