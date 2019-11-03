61e6ead391233490aa1e491e3a30b84cea2e8cbb
Benedikt Magnus <magnus@magnuscraft.de>
2019-11-03 12:53:44 +0100

    Tests for stateless commands.

---
+++ tests/tests/messages/statelessCommands.ts
@@ -0,0 +1,96 @@
+import 'mocha';
+import * as assert from 'assert';
+
+import ConfigTestUtility from '../../utility/config';
+import { TestMessageWithFixedAuthor } from '../../utility/message';
+
+import Localisation from '../../../scripts/utility/localisation';
+import Database from '../../../scripts/wichtelbot/database';
+import Config from '../../../scripts/utility/config';
+
+import MessageHandler from '../../../scripts/wichtelbot/message/handler';
+
+import User from '../../../scripts/wichtelbot/message/definitions/user';
+import { ChannelType } from '../../../scripts/wichtelbot/message/definitions/channel';
+
+describe('statelessCommands',
+    function ()
+    {
+        let database: Database;
+        let messageHandler: MessageHandler;
+
+        before(
+            function ()
+            {
+                // Set event phase to "registration" as a basis to work with:
+                ConfigTestUtility.setToRegistrationPhase();
+
+                // Initialise dependencies:
+                database = new Database('mainTest', 'logTest', true);
+                messageHandler = new MessageHandler(database);
+
+                const resultCallback = (): void => {};
+
+                // Make first contact so we are known:
+                const message = new TestMessageWithFixedAuthor(resultCallback, resultCallback, resultCallback, ChannelType.Server);
+                message.content = Config.main.commandPrefix + Localisation.commands.contacting.commands[0];
+                messageHandler.process(message);
+            }
+        );
+
+        after(
+            function ()
+            {
+                database.close();
+
+                ConfigTestUtility.resetConfig();
+            }
+        );
+
+        it('goodMorning',
+            function ()
+            {
+                let called = false;
+                let author: User;
+
+                const resultCallback = (text: string): void =>
+                {
+                    assert.strictEqual(text, Localisation.texts.goodMorning.process(author));
+                    called = true;
+                };
+
+                const message = new TestMessageWithFixedAuthor(resultCallback, resultCallback, resultCallback, ChannelType.Personal);
+                message.content = Localisation.commands.goodMorning.commands[0];
+
+                author = message.author;
+
+                messageHandler.process(message);
+
+                assert.strictEqual(called, true);
+            }
+        );
+
+        it('maybe',
+            function ()
+            {
+                let called = false;
+                let author: User;
+
+                const resultCallback = (text: string): void =>
+                {
+                    assert.strictEqual(text, Localisation.texts.maybeResponse.process(author));
+                    called = true;
+                };
+
+                const message = new TestMessageWithFixedAuthor(resultCallback, resultCallback, resultCallback, ChannelType.Personal);
+                message.content = Localisation.commands.maybe.commands[0];
+
+                author = message.author;
+
+                messageHandler.process(message);
+
+                assert.strictEqual(called, true);
+            }
+        );
+    }
+);
