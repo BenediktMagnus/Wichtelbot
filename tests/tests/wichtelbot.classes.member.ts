1a7dc20b66bf6d9d9821fd129174960679eb035d
Benedikt Magnus <magnus@magnuscraft.de>
2019-11-05 12:34:49 +0100

    More tests for the initialisation of the Member class.

--- tests/tests/wichtelbot.classes.member.ts
+++ tests/tests/wichtelbot.classes.member.ts
@@ -1,14 +1,33 @@
 import 'mocha';
 import { assert } from 'chai';
 
-import Member from '../../scripts/wichtelbot/classes/member';
 import ContactTestUtility from '../utility/contact';
 import ContactType from '../../scripts/wichtelbot/types/contactType';
+import Member from '../../scripts/wichtelbot/classes/member';
+import GeneralTestUtility from '../utility/general';
 
 describe('member class',
     function ()
     {
-        it('fromContact',
+        it('from ContactData',
+            function ()
+            {
+                const contactData = ContactTestUtility.createRandomContactData();
+
+                const member = new Member(contactData);
+
+                // The new information must be present:
+                assert.strictEqual(member.information.contactId, contactData.id);
+
+                assert.deepNestedInclude(member, contactData);
+
+                // Only changing the class must not change the type.
+                // See "docs/Concepts.md -> Contact types vs contact classes" for more information.
+                assert.strictEqual(member.type, ContactType.Contact);
+            }
+        );
+
+        it('from Contact',
             function ()
             {
                 const contact = ContactTestUtility.createRandomContact();
@@ -16,13 +35,41 @@ describe('member class',
                 const member = new Member(contact);
 
                 // The new information must be present:
-                assert.notStrictEqual(member.information, undefined);
-                assert.notStrictEqual(member.information, null);
+                assert.strictEqual(member.information.contactId, contact.id);
 
-                assert.strictEqual(member.id, contact.id);
-                assert.strictEqual(member.state, contact.state);
+                assert.deepNestedInclude(member, contact);
 
-                assert.strictEqual(member.type, ContactType.Member);
+                // Only changing the class must not change the type.
+                // See "docs/Concepts.md -> Contact types vs contact classes" for more information.
+                assert.strictEqual(member.type, ContactType.Contact);
+            }
+        );
+
+        it('from Contact with Information',
+            function ()
+            {
+                const contact = ContactTestUtility.createRandomContact();
+                const information = ContactTestUtility.createRandomMemberInformation(contact.id);
+
+                const member = new Member(contact, information);
+
+                assert.deepStrictEqual(member.information, information);
+
+                assert.deepNestedInclude(member, contact);
+            }
+        );
+
+        it('overwrites information ID.',
+            function ()
+            {
+                const contact = ContactTestUtility.createRandomContact();
+
+                const randomId = GeneralTestUtility.createRandomString();
+                const information = ContactTestUtility.createRandomMemberInformation(randomId);
+
+                const member = new Member(contact, information);
+
+                assert.deepStrictEqual(member.information.contactId, contact.id);
             }
         );
     }
