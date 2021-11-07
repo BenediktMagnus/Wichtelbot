4f7daba051d8998c6d2154693dd6115e169deb9f
Benedikt Magnus <magnus@magnuscraft.de>
2021-11-07 17:44:38 +0100

    Removed unnecessary parts of the old JS code.

--- Skripte/Nachrichten.js
+++ Skripte/Nachrichten.js
@@ -1,16 +1,6 @@
 export default class MessageHandler
 {
     Definitionen = {
-        //Jederzeit möglich:
-        Befehle: {
-            "hilfe": {
-                Funktion: ModulAllgemein.HilfeRufen,
-                Text: Texte.Hilfe
-            },
-            "sternenrose": {
-                Funktion: ModulAllgemein.Sternenrose
-            }
-        },
         //Nur im Moderationskanal:
         Moderation: {
             "info": {
@@ -56,130 +46,6 @@ export default class MessageHandler
         },
         //Nur in einem bestimmten Zustand gültig:
         Zustände: {
-            Registrierung: {
-                "ja": {
-                    Funktion: ModulAllgemein.Fortfahren,
-                    Ziel: "AnalogDigitalWichtel",
-                    Text: Texte.AnalogDigitalWichtel
-                },
-                "nein": {
-                    Aliase: ["nö", "ne"],
-                    Funktion: ModulAllgemein.Fortfahren,
-                    Ziel: "Neu",
-                    Text: Texte.Kontaktaufnahme
-                },
-                "vielleicht" : {
-                    Aliase: ["vllt", "eventuell", "evtl"],
-                    Funktion: ModulAllgemein.Antworten,
-                    Text: Texte.Vielleicht
-                }
-            },
-            AnalogDigitalWichtel: {
-                "analog": {
-                    Aliase: ["digital", "beides"],
-                    Funktion: ModulDatenaufnahme.DatenAufnehmen,
-                    Ziel: "AnalogDigitalSelbst",
-                    Text: Texte.AnalogDigitalSelbst
-                }
-            },
-            AnalogDigitalSelbst: {
-                "analog": {
-                    Aliase: ["beides"],
-                    Funktion: ModulDatenaufnahme.DatenAufnehmen,
-                    Ziel: "Anschrift",
-                    Text: Texte.Anschrift
-                },
-                "digital": {
-                    Funktion: ModulDatenaufnahme.DatenAufnehmen,
-                    Ziel: "Steam",
-                    Text: Texte.Steam
-                }
-            },
-            Anschrift: {
-                Datenaufnahme: true,
-                Funktion: ModulDatenaufnahme.DatenAufnehmen,
-                Ziel: "Land",
-                Text: Texte.Land
-            },
-            Land: {
-                "deutschland": {
-                    Aliase: ["österreich", "schweiz", "luxemburg"],
-                    Funktion: ModulDatenaufnahme.LandVerarbeiten
-                }
-            },
-            Steam: {
-                Datenaufnahme: true,
-                Funktion: ModulDatenaufnahme.SteamVerarbeiten
-            },
-            International: {
-                "ja": {
-                    Aliase: ["nein", "nö", "ne"],
-                    Funktion: ModulDatenaufnahme.DatenAufnehmen,
-                    Ziel: "Wunschliste",
-                    Text: Texte.Wunschliste
-                },
-                "vielleicht" : {
-                    Aliase: ["vllt", "eventuell", "evtl"],
-                    Funktion: ModulAllgemein.Antworten,
-                    Text: Texte.Vielleicht
-                }
-            },
-            Wunschliste: {
-                Datenaufnahme: true,
-                Funktion: ModulDatenaufnahme.WunschlisteVerarbeiten
-            },
-            Allergien: {
-                Datenaufnahme: true,
-                Funktion: ModulDatenaufnahme.DatenAufnehmen,
-                Ziel: "AusschlussGeschenk",
-                Text: Texte.AusschlussGeschenk
-            },
-            AusschlussGeschenk: {
-                Datenaufnahme: true,
-                Funktion: ModulDatenaufnahme.DatenAufnehmen,
-                Ziel: "AusschlussWichtel",
-                Text: Texte.AusschlussWichtel
-            },
-            AusschlussWichtel: {
-                Datenaufnahme: true,
-                Funktion: ModulDatenaufnahme.DatenAufnehmen,
-                Ziel: "Freitext",
-                Text: Texte.Freitext
-            },
-            Freitext: {
-                Datenaufnahme: true,
-                Funktion: ModulDatenaufnahme.DatenAufnehmen,
-                Ziel: "Teilnehmer",
-                Text: Texte.Teilnehmer
-            },
-            Teilnehmer: {
-                "ändern": {
-                    Funktion: ModulAllgemein.Fortfahren,
-                    Ziel: "ÄnderungBestätigen",
-                    Text: Texte.ÄnderungBestätigen
-                }
-            },
-            ÄnderungBestätigen: {
-                "ja": {
-                    Funktion: ModulDatenaufnahme.DatenÄndern,
-                    Ziel: "AnalogDigitalWichtel",
-                    Text: Texte.AnalogDigitalWichtel
-                },
-                "nein": {
-                    Aliase: ["nö", "ne"],
-                    Funktion: ModulAllgemein.Fortfahren,
-                    Ziel: "Teilnehmer",
-                    Text: Texte.ÄnderungAbgebrochen
-                },
-                "vielleicht" : {
-                    Aliase: ["vllt", "eventuell", "evtl"],
-                    Funktion: ModulAllgemein.Antworten,
-                    Text: Texte.Vielleicht
-                }
-            },
-            Wartend: {
-                //Leer
-            },
             Wichtel: {
                 "anonym meinem wichtelpaten schreiben": {
                     Funktion: ModulAllgemein.Fortfahren,
@@ -222,9 +88,6 @@ export default class MessageHandler
                 Datenaufnahme: true,
                 Funktion: ModulPakete.EmpfangenDatumAufnehmen
             }
-        },
-        NichtVerstanden: {
-            Funktion: ModulAllgemein.NichtVerstanden
         }
     };
 }
--- Skripte/Nachrichten/Allgemein.js
+++ Skripte/Nachrichten/Allgemein.js
@@ -1,41 +1,4 @@
 /**
- * Gibt zustandsabhängige Informationen aus, wenn die Eingabe nicht verstanden wurde.
- * @param {Object} Nachricht Die Nachricht, die per Discord erhalten wurde, ein Discordnachrichtenobjekt.
- * @param {Object} Nutzer Das Nutzerobjekt mit allen Angaben zum Nutzer.
- */
-function NichtVerstanden (Nachricht, Nutzer)
-{
-    let Antwort = Texte.NichtVerstanden;
-
-    Antwort += "\n\n" + Texte.InfoImmer;
-
-    if (Nutzer.Zustand == 'Teilnehmer')
-        Antwort += "\n" + Texte.InfoTeilnehmer;
-    else if (Nutzer.Zustand == 'Wichtel')
-        Antwort += "\n" + Texte.InfoWichtel;
-
-    Nachricht.reply(Antwort);
-}
-exports.NichtVerstanden = NichtVerstanden;
-
-/**
- * Informiert die Orga im privaten Kanal über benötigte Hilfe.
- * @param {Object} Nachricht Die Nachricht, die per Discord erhalten wurde, ein Discordnachrichtenobjekt.
- * @param {Object} Nutzer Das Nutzerobjekt mit allen Angaben zum Nutzer.
- * @param {Object} Befehlsobjekt Das Befehlsobjekt der Zustandsdefinition, das gerade ausgeführt wird.
- */
-function HilfeRufen (Nachricht, Nutzer, Befehlsobjekt)
-{
-    let Orgainformation = Texte.HilfeOrgainformation.replace(/\[\[NUTZERNAME\]\]/g, Nutzer.Name);
-    Orgainformation = Orgainformation.replace(/\[\[NUTZERTAG\]\]/g, Nutzer.Discord);
-
-    Klient.channels.get(Config.KanalIdOrganisation).send(Orgainformation);
-
-    Antworten(Nachricht, Nutzer, Befehlsobjekt);
-}
-exports.HilfeRufen = HilfeRufen;
-
-/**
  * Sendet anonym eine Nachricht vom Wichtelkind an seinen Wichtelpaten.
  * @param {Object} Nachricht Die Nachricht, die per Discord erhalten wurde, ein Discordnachrichtenobjekt.
  * @param {Object} Nutzer Das Nutzerobjekt mit allen Angaben zum Nutzer.
@@ -82,13 +45,3 @@ function NachrichtAnWichtelkind (Nachricht, Nutzer)
     );
 }
 exports.NachrichtAnWichtelkind = NachrichtAnWichtelkind;
-
-/**
- * Informiert die Orga im privaten Kanal über benötigte Hilfe.
- * @param {Object} Nachricht Die Nachricht, die per Discord erhalten wurde, ein Discordnachrichtenobjekt.
- */
-function Sternenrose (Nachricht)
-{
-    Nachricht.reply(Texte.Sternenrose, BildSternenrose);
-}
-exports.Sternenrose = Sternenrose;
